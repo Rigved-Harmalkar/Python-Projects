9747a1c64cf713f33e794324d22d724a16fd619f
Rigved-Harmalkar <89383329+Rigved-Harmalkar@users.noreply.github.com>
2023-02-04 17:00:26 +0530

    Add files via upload

---
+++ hangman/main.py
@@ -0,0 +1,101 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["Dadar", "bandra", "malad"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+
+#Setting lives to equal 6.
+lives = 6
+
+
+
+#Create a list
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+       
+        if letter == guess:
+            display[position] = letter
+
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+
+    print(f"{' '.join(display)}")
+
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+
+    print(stages[lives])
---
+++ hangman/readme.txt
@@ -0,0 +1,4 @@
+A basic Hangman game 
+Using basic python logic this game was created
+will help you in building basic python knowledge
+Good program to practice your skills.
